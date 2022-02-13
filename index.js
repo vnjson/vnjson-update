6bb27befd47d6d78c75621cacb8b55972dfc1d2a
kserks <bakakasin@gmail.com>
2022-02-13 14:58:14 +0600

    bin

--- index.js
+++ index.js
@@ -3,27 +3,31 @@ const diff = require('diff-arrays-of-objects');
 const path = require('path')
 const getHash = require('./lib/get-hash.js')
 const getUpdate = require('./lib/get-update.js')
-
+const compare = require('./lib/compare.js')
 
 
 //const original = require('./db/original.json')
 
 async function updateVnjson(config){
   //  fs.remove(extractDir, ()=>{})
-  await getUpdate(config.zip)
+  await getUpdate(config)
   console.log('[ extract zip ]')
 
   
+  const _update = await fs.readJson(config.update)
+  const _local = await fs.readJson(config.local)
 
-  let update = await getHash(config.update)
-  let local = await getHash( config.local)
   //fs.writeJSON(path.resolve(config.zip+'/update.json' ), update, { spaces: 2 } )
-  let result = diff(update, local, 'hash')
+  let existUpdate = diff(_update, _local, 'hash')
+  if(existUpdate.removed.length===0){
+    console.log('[+] Установлена последняя версия')
+  }
+  else{
+    const _changes = await getHash( config.changes)
+    compare (_update, _local, _changes)
+  }
   //fs.writeJSON(path.resolve(config.zip+'/local-change.json' ), result, { spaces: 2 } )
-  console.log('update-change')
-  console.log(result.removed)
-  console.log('local-change')
-  console.log(result.added)
+
 }
 
 
