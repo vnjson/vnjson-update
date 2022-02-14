a8050b9f3794abcfa8c4abba4a56785b82cee77a
kserks <bakakasin@gmail.com>
2022-02-14 19:11:36 +0600

    latest

--- index.js
+++ index.js
@@ -5,39 +5,28 @@ const getHash = require('./lib/get-hash.js')
 const getUpdate = require('./lib/get-update.js')
 const compare = require('./lib/compare.js')
 
+async function updateVnjson(localProject){
 
-//const original = require('./db/original.json')
-
-async function updateVnjson(config){
-  //  fs.remove(extractDir, ()=>{})
+  const config = {
+      url: 'https://github.com/vnjson/mcap/archive/refs/heads/main.zip',
+      zip: '.vnjson',
+      update: '.vnjson/mcap-main',
+      local: localProject
+  }
   await getUpdate(config)
   console.log('[ extract zip ]')
 
-  
-  const _update = await fs.readJson(config.update)
-  const _local = await fs.readJson(config.local)
 
-  //fs.writeJSON(path.resolve(config.zip+'/update.json' ), update, { spaces: 2 } )
-  let existUpdate = diff(_update, _local, 'hash')
-  if(existUpdate.removed.length===0){
-    console.log('[+] Установлена последняя версия')
-  }
-  else{
-    const _changes = await getHash( config.changes)
-    compare (_update, _local, _changes)
-  }
-  //fs.writeJSON(path.resolve(config.zip+'/local-change.json' ), result, { spaces: 2 } )
+    const _update = await getHash(config.update)
+    const _local = await getHash(config.local)
+
+    compare(_update, _local, config)
+
+
+
+
 
 }
 
-
 module.exports = updateVnjson
-//
-/*
-added:
-changed:
-deprecated:
-removed:
-fixed
- */
 
